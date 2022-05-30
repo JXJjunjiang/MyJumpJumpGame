9f67a74b371a764818b9d501943301283fe9d2f3
JunJiang <2417306980@qq.com>
2022-05-30 23:19:03 +0800

    dev
    新建仓库并上传项目

---
+++ Assets/Scripts/Controll.cs
@@ -0,0 +1,247 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Controll : MonoBehaviour
+{
+    private const float MinScaleY = 0.2f, MaxScaleY = 2f;
+    private const float YDownSpeed = 0.1f, YUpSpeed = 0.3f;
+    private const float JumpY = 8f;
+    private const float JumpHorizontalSpeed = 15f;
+    private static Vector2 platformSize;
+    private const float FailedPosY = 1f;
+
+    private Transform curPlatform, nextPlatform;
+    private Rect[] jugeArea;
+    private Transform character;
+
+    private Vector3 cam2playerDir;
+    private float cam2playerDistance;
+
+    bool isPress;
+    bool isJump;
+    bool canControll;
+    Vector3 startPoint, midPoint, endPoint;
+    float jumpTime = 0;
+
+    private void Start()
+    {
+        curPlatform = GameObject.Find("Platform").transform;
+        nextPlatform = null;
+        character = GameObject.Find("Player").transform;
+        platformSize.Set(GameManager.Instance.PlatformPrefabSize.x, GameManager.Instance.PlatformPrefabSize.z);
+        jugeArea = new Rect[2] { new Rect(), new Rect() };//第0个代表现在的platform，第1个代表下一个platform
+
+        GameStart();
+    }
+
+    void GameStart()
+    {
+        Spawn(curPlatform);
+        canControll = true;
+        cam2playerDir = (Camera.main.transform.position - character.position).normalized;
+        cam2playerDistance = Vector3.Distance(character.position, Camera.main.transform.position);
+        Debug.Log(character.position + cam2playerDir * cam2playerDistance);
+    }
+
+    void GameEnd()
+    {
+        canControll = false;
+    }
+
+    void Spawn(Transform next)
+    {
+        JumpTrs jt = GameManager.Instance.GeneratePlatform(next);
+        curPlatform = jt.currentTrs;
+        nextPlatform = jt.nextTrs;
+    }
+
+    void PlatformDown()
+    {
+        //设置scale
+        Vector3 scale = curPlatform.localScale;
+        float scaleY = Mathf.Clamp(scale.y - YDownSpeed, MinScaleY, MaxScaleY);
+        scale.Set(scale.x, scaleY, scale.z);
+        curPlatform.localScale = scale;
+        //设置position
+        SetPlatformYPos(scale);
+    }
+
+    void PlatformUp()
+    {
+        //设置scale
+        Vector3 scale = curPlatform.localScale;
+        float scaleY = Mathf.Clamp(scale.y + YUpSpeed, MinScaleY, MaxScaleY);
+        scale.Set(scale.x, scaleY, scale.z);
+        curPlatform.localScale = scale;
+        //判断是否可以起跳
+        if (Mathf.Abs(MaxScaleY-scale.y)<=0.1f)
+        {
+            isPress = false;
+        }
+        //设置position
+        SetPlatformYPos(scale);
+    }
+
+    void SetPlatformYPos(Vector3 scale)
+    {
+        Vector3 pos = curPlatform.localPosition;
+        pos.Set(pos.x, scale.y / 2f, pos.z);
+        curPlatform.localPosition = pos;
+    }
+
+    private void Update()
+    {
+        if (!canControll)
+            return;
+
+        if (Input.GetMouseButtonDown(0))//按下鼠标时刷新贝塞尔的三个点
+        {
+            RefreshThreePoint();
+            GetCurrentArea();
+            GetNextArea();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            PlatformDown();
+            isPress = true;
+            SetThreePoint();
+        }
+        else if(isPress)
+        {
+            PlatformUp();
+            if (!isPress)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(startPoint,0.2f);
+        Gizmos.DrawSphere(midPoint, 0.2f);
+        Gizmos.DrawSphere(endPoint, 0.2f);
+    }
+
+    #region EndPointMove
+
+    void RefreshThreePoint()
+    {
+        startPoint = character.position;
+        Vector3 temp = new Vector3(nextPlatform.localPosition.x, startPoint.y, nextPlatform.localPosition.z);
+        Vector3 direction = (temp - startPoint).normalized;
+        endPoint = character.position + direction;
+        float midX = startPoint.x+(endPoint.x - startPoint.x) / 2f;
+        float midZ = startPoint.z+(endPoint.z - startPoint.z) / 2f;
+        midPoint.Set(midX, JumpY, midZ);
+    }
+    void SetThreePoint()
+    {
+        Vector3 temp = new Vector3(nextPlatform.localPosition.x, startPoint.y, nextPlatform.localPosition.z);
+        Vector3 direction = (temp - startPoint).normalized;
+        endPoint += (direction * 0.1f);
+        float midX = startPoint.x + (endPoint.x - startPoint.x) / 2f;
+        float midZ = startPoint.z + (endPoint.z - startPoint.z) / 2f;
+        midPoint.Set(midX, JumpY, midZ);
+    }
+    #endregion
+
+    #region Jump
+    void Jump()
+    {
+        float distance = Vector3.Distance(endPoint, startPoint);
+        jumpTime =  distance/ JumpHorizontalSpeed;
+        canControll = false;
+        DOTween.To((t) =>
+        {
+            SetCharacterCurveMove(t);
+            SetCharacterRotate(t);
+        }, 0f, 1f, jumpTime).onComplete = OnJumpFinish;
+    }
+
+    void SetCharacterCurveMove(float t)
+    {
+        Vector3 targetPoint = GetBezierPoint(t);
+        character.position = targetPoint;
+    }
+
+    void SetCharacterRotate(float t)
+    {
+        Vector3 rotation = character.localEulerAngles;
+        rotation.Set(rotation.x, rotation.y, t * 360f);
+        character.localEulerAngles = rotation;
+    }
+
+    public Vector3 GetBezierPoint(float t)
+    {
+        Vector3 a = startPoint;
+        Vector3 b = midPoint;
+        Vector3 c = endPoint;
+
+        Vector3 aa = a + (b - a) * t;
+        Vector3 bb = b + (c - b) * t;
+        return aa + (bb - aa) * t;
+    }
+    #endregion
+
+    #region JumpFinish
+    void OnJumpFinish()
+    {
+        if (IsInArea())
+        {
+            Spawn(nextPlatform);
+            canControll = true;
+            CameraFocus();
+        }
+    }
+    #endregion
+
+    void GetNextArea()
+    {
+        Vector3 nextPos = nextPlatform.localPosition;
+        GetArea(nextPos, 1);
+    }
+
+    void GetCurrentArea()
+    {
+        Vector3 curPos = curPlatform.localPosition;
+        GetArea(curPos, 0);
+    }
+
+    void GetArea(Vector3 pos,int index)
+    {
+        Vector2 rectStartPos = new Vector2(pos.x - platformSize.x / 2, pos.z - platformSize.y / 2);
+        jugeArea[index].Set(rectStartPos.x, rectStartPos.y, platformSize.x, platformSize.y);
+    }
+    
+    bool IsInArea()
+    {
+        for (int i = 0; i < jugeArea.Length; i++)
+        {
+            if (jugeArea[i].Contains(new Vector2(endPoint.x, endPoint.z)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void CameraFocus()
+    {
+        Vector3 camEndPos = character.position + cam2playerDistance * cam2playerDir;
+        DOTween.To((t) =>
+        {
+            CameraMove(camEndPos, t);
+        }, 0, 1, 0.5f);
+    }
+
+    void CameraMove(Vector3 camEndPos,float t)
+    {
+        Vector3 dir = (camEndPos - Camera.main.transform.position).normalized;
+        float distance = Vector3.Distance(camEndPos, Camera.main.transform.position);
+        Camera.main.transform.position += dir * distance * t;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+
+public class GameManager : Singalton<GameManager>
+{
+    private const string PlatformPrefabName = "Platform";
+    public readonly Vector3 PlatformPrefabSize = new Vector3(3f, 2f, 3f);
+    private readonly Vector2 ClampSpawnX = new Vector2(-10f, -5f);
+    private readonly Vector2 ClampSpawnZ = new Vector2(5f, 10f);
+    private readonly float SpawnY = 1f;
+
+    private GameObject platformPrefab = null;
+    private Transform platformRoot = null;
+    private List<Transform> spawnPlatform;
+
+    protected override void Awake ()
+    {
+        base.Awake();
+        platformPrefab = Resources.Load(PlatformPrefabName) as GameObject;
+        platformRoot = GameObject.Find("PlatformRoot").transform;
+        spawnPlatform = new List<Transform>();
+        spawnPlatform.Add(GameObject.Find("Platform").transform);
+    }
+
+    /// <summary>
+    /// 生成跳跃平台
+    /// </summary>
+    /// <param name="spawnPos"></param>
+    /// <returns></returns>
+    public JumpTrs GeneratePlatform(Transform prePlatform)
+    {
+        Vector3 spawnPos = GetNextPlatformPos(prePlatform.localPosition);
+        Transform result = Instantiate<GameObject>(platformPrefab, platformRoot).transform;
+        spawnPlatform.Add(result);
+        result.Reset();
+        result.localScale = PlatformPrefabSize;
+        result.localPosition = new Vector3(spawnPos.x, -SpawnY, spawnPos.z);
+        result.DOLocalMoveY(SpawnY, 0.2f);
+
+        return new JumpTrs(SearchSpawnList(prePlatform.name), result);
+    }
+
+    Vector3 GetNextPlatformPos(Vector3 prePos)
+    {
+        Vector3 result = Vector3.zero;
+        float x = 0;
+        float z = 0;
+        if (Random.Range(1,2)==1)
+        {
+            x = Random.Range(ClampSpawnX.x, ClampSpawnX.y);
+        }
+        else
+        {
+            z = Random.Range(ClampSpawnZ.x, ClampSpawnZ.y);
+        }
+        result.Set(prePos.x + x, SpawnY, prePos.z + z);
+        return result;
+    }
+
+    Transform SearchSpawnList(string name)
+    {
+        using (var e=spawnPlatform.GetEnumerator())
+        {
+            while (e.MoveNext())
+            {
+                if (e.Current.name==name)
+                {
+                    return e.Current;
+                }
+            }
+        }
+        return null;
+    }
+}
+
+public class Singalton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get => _instance;
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else
+        {
+            _instance = null;
+            DestroyImmediate(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+public static class UnityExtension
+{
+    public static void Reset(this Transform trs)
+    {
+        trs.localScale = Vector3.one;
+        trs.localPosition = Vector3.zero;
+        trs.localRotation = Quaternion.identity;
+    }
+}
+public class JumpTrs
+{
+    public Transform currentTrs;
+    public Transform nextTrs;
+    public JumpTrs(Transform cur,Transform next)
+    {
+        this.currentTrs = cur;
+        this.nextTrs = next;
+    }
+    private JumpTrs()
+    {
+
+    }
+}
